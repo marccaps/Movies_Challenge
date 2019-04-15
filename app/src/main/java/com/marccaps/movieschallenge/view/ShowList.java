920a604c79a3da91a61ddca3a19f4be611e935ad
marc <marccaps@gmail.com>
2019-04-15 21:59:57 +0200

    Shimmer Loading effect

--- app/src/main/java/com/marccaps/movieschallenge/view/ShowList.java
+++ app/src/main/java/com/marccaps/movieschallenge/view/ShowList.java
@@ -1,4 +1,15 @@
 package com.marccaps.movieschallenge.view;
 
-public class ShowList {
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.marccaps.movieschallenge.R;
+
+public class ShowList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.show_list_layout);
+        super.onCreate(savedInstanceState);
+    }
 }
